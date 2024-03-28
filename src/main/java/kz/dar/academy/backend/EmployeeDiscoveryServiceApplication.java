582c78f7ced20c2f4b9fd8bda75665b6b9dfadfe
khdinarakh <nis.dinara2002@gmial.com>
2024-03-28 13:52:12 +0600

    first commit

---
+++ src/main/java/kz/dar/academy/backend/EmployeeDiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package kz.dar.academy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EmployeeDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDiscoveryServiceApplication.class, args);
+    }
+
+}
